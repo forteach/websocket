2b75acecf543fcf6da1d673bc6e83d79aa104907
lzm <1119264845@qq.com>
2019-01-02 17:08:29 +0800

    feat add Header

---
+++ src/main/java/com/forteach/websocket/config/SimpleCORSFilter.java
@@ -0,0 +1,37 @@
+package com.forteach.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Description:
+ * @author: liu zhenming
+ * @version: V1.0
+ * @date: 2019/1/2  17:02
+ */
+@Configuration
+public class SimpleCORSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest requestToUse = (HttpServletRequest) servletRequest;
+        HttpServletResponse responseToUse = (HttpServletResponse) servletResponse;
+
+        responseToUse.addHeader("Sec-WebSocket-Protocol", ((HttpServletRequest) servletRequest).getHeader("Sec-WebSocket-Protocol"));
+        filterChain.doFilter(requestToUse, responseToUse);
+    }
+}
