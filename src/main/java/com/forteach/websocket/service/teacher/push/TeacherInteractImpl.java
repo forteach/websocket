514a202c2ce0ff7211cda5704ccd3b359f5fd7b2
zjw <you@example.com>
2019-03-20 14:46:57 +0800

    update classRoom stu 去重

--- src/main/java/com/forteach/websocket/service/teacher/push/TeacherInteractImpl.java
+++ src/main/java/com/forteach/websocket/service/teacher/push/TeacherInteractImpl.java
@@ -68,8 +68,8 @@ public class TeacherInteractImpl {
                 .members(ClassRoomKey.getInteractiveIdQra(circleId))
                 .stream()
                 .filter(id -> !id.equals(teacherId))//需要过滤掉教师ID
-                .filter(id->hasJoin(circleId,id))
-                .map(id->joinStuTuiSong(circleId,id))
+               // .filter(id->hasJoin(circleId,id))
+               // .map(id->joinStuTuiSong(circleId,id))
                 .collect(Collectors.toList());
     }
 
